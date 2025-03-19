d4469b6fe34737ee1c4c1c1df82662d65c644b1b
testgithubsonika <tomarsonika01@gmail.com>
2025-03-19 19:25:33 +0530

    fastapi :Enhance model loading and add TensorFlow GPU memory management; update class labels for plant disease detection

--- main.py
+++ main.py
@@ -36,9 +36,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# TensorFlow memory management
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
 # Load the trained TensorFlow model
 try:
-    model = tf.keras.models.load_model("C:/Users/hp/Desktop/cyberpunker/papaya_model.h5")
+    model = tf.keras.models.load_model("models/plant_leaf_disease_detector.keras")
     print("Model loaded successfully")
 except Exception as e:
     print(f"Error loading model: {e}")
@@ -77,7 +86,20 @@ async def predict(file: UploadFile = File(...)):
         print("Predicted Class Index:", predicted_class)
 
         # Define class labels (modify according to your trained model)
-        class_labels = ["Healthy", "Disease", "Weather Issue", "Other", "class-5", "class-6"]
+        class_labels = [
+            "Apple___Apple_scab", "Apple___Black_rot", "Apple___Cedar_apple_rust", "Apple___healthy",
+            "Blueberry___healthy", "Cherry_(including_sour)___Powdery_mildew", "Cherry_(including_sour)___healthy",
+            "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot", "Corn_(maize)___Common_rust_",
+            "Corn_(maize)___Northern_Leaf_Blight", "Corn_(maize)___healthy", "Grape___Black_rot",
+            "Grape___Esca_(Black_Measles)", "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)", "Grape___healthy",
+            "Orange___Haunglongbing_(Citrus_greening)", "Peach___Bacterial_spot", "Peach___healthy",
+            "Pepper,_bell___Bacterial_spot", "Pepper,_bell___healthy", "Potato___Early_blight", "Potato___Late_blight",
+            "Potato___healthy", "Raspberry___healthy", "Soybean___healthy", "Squash___Powdery_mildew",
+            "Strawberry___Leaf_scorch", "Strawberry___healthy", "Tomato___Bacterial_spot", "Tomato___Early_blight",
+            "Tomato___Late_blight", "Tomato___Leaf_Mold", "Tomato___Septoria_leaf_spot",
+            "Tomato___Spider_mites Two-spotted_spider_mite", "Tomato___Target_Spot",
+            "Tomato___Tomato_Yellow_Leaf_Curl_Virus", "Tomato___Tomato_mosaic_virus", "Tomato___healthy"
+        ]
 
         # Handle cases where the predicted class index is out of range
         if predicted_class >= len(class_labels):
